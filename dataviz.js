dac9035bc62548a029b46af1525793bcab23f4b3
alinem <158090981+alinemaligorne@users.noreply.github.com>
2024-03-18 11:47:56 +0100

    ajout fichiers
    fichiers html, js, css

---
+++ dataviz.js
@@ -0,0 +1,7 @@
+let time = Date();
+console.log(time)
+const dateElement = document.body.innerHTML += "<p>" + time + "</p>"
+// document.getElementById("date").innerText = time
+// dateElement.innerText = time
+
+
