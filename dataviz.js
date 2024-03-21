3161bedb06c276ff52366b73c511bc76dc0052ed
ModusGit <157380395+ModusGit@users.noreply.github.com>
2024-03-21 15:32:06 +0100

    V1
    Version 1 du projet

--- dataviz.js
+++ dataviz.js
@@ -1,7 +1,11 @@
-let time = Date();
-console.log(time)
-const dateElement = document.body.innerHTML += "<p>" + time + "</p>"
-// document.getElementById("date").innerText = time
-// dateElement.innerText = time
+fetch ("https://ws.audioscrobbler.com/2.0/?method=tag.getTopTags&api_key=https://ws.audioscrobbler.com/2.0/?method=tag.getTopTags&api_key=676d5b220e27f2ab3683c0f4b95bbd5b&format=json&format=json")
+    .then((response) => {
+        return response.json();
+        console.log(response);
+    })
+    .then((json) => {
+        console.log(json);
+    });
 
+const styleMusic
 
