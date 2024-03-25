513f7fbd1f9b6d5279cd6474b0ac4429ea9eb23d
alinem <158090981+alinemaligorne@users.noreply.github.com>
2024-03-25 16:16:19 +0100

    New version
    clean code + mise à jour du tableau genres

--- js/chartbase.js
+++ js/chartbase.js
@@ -5,31 +5,43 @@ const collection = await fetch("https://ws.audioscrobbler.com/2.0/?method=tag.ge
 const music = await collection.json();
 // On récuère le chemin exacte pour n'obtneir plus qu'un tableau
 const musicTags = music.toptags.tag;
+
+
+function labelFetch() {
+  let genres = [];
+  for (let i = 0; i < musicTags.length; i++) {
+    genres.push(musicTags[i]["name"]);
+    
+  }
+  return genres;
+}
+console.log(labelFetch())
 const rock = (musicTags[0]["count"])/50000;
 const electro = (musicTags[1]["count"])/50000;
 const pop = (musicTags[5]["count"])/50000;
-console.log(musicTags);
+
 
 new Chart(ctx, {
   type: 'bubble', 
   data: {
+    labels: labelFetch(),
     datasets: [
       {
-        label: 'Rock',
+        //label: 'Rock',
         data: [{
           x: 3,
           y: 4,
           r: rock
+        },
+        {
+          x: 3,
+          y: 3,
+          r: 45
+        }, {
+          x: 5,
+          y: 9,
+          r: 49
         }
-        // {
-        //   x: 3,
-        //   y: 3,
-        //   r: 45
-        // }, {
-        //   x: 5,
-        //   y: 9,
-        //   r: 49
-        // }
       ],
         borderColor: '#de9eb2',
         borderWidth: 3,
@@ -38,48 +50,49 @@ new Chart(ctx, {
         //drawActiveElementsOnTop
       },
       {
-        label : 'Electronic',
+        //label : 'Electronic',
         data: [{
           x: 4,
           y: 4,
           r: electro
+        },
+        {
+          x: 7,
+          y: 7,
+          r: 9
+        }, 
+        {
+          x: 15,
+          y: 6,
+          r: 65
         }
-        // {
-        //   x: 7,
-        //   y: 7,
-        //   r: 9
-        // }, 
-        // {
-        //   x: 15,
-        //   y: 6,
-        //   r: 65
-        // }
       ],
         borderColor: '#82bfe8',
         borderWidth: 3,
         backgroundColor: 'rgb(54,162,235)',
       },
       {
-        label : 'Pop',
+        //label : 'Pop',
         data: [{
           x: 9,
           y: 6,
           r: pop
+        },
+        {
+          x: 11,
+          y: 4,
+          r: 35
+        }, 
+        {
+          x: 5,
+          y: 6,
+          r: 32
         }
-        // {
-        //   x: 11,
-        //   y: 4,
-        //   r: 35
-        // }, 
-        // {
-        //   x: 5,
-        //   y: 6,
-        //   r: 32
-        // }
       ],
         borderColor: '#f0e0bb',
         borderWidth: 3,
         backgroundColor: 'rgb(255,205,86)',
+
       }
     ]
   },
